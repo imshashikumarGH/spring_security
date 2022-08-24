53c05d53fa3780276a810b09647475272496fed6
imshashikumarGH <imshashikumar01@gmail.com>
2022-08-24 22:21:52 +0530

    Feature DB creation : Insert Command in data.sql file

---
+++ user-service/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO person (p_id, name, designation, address) VALUES ('shashi@gmail.com', 'shashi', 'SE','USA');
+INSERT INTO person (p_id, name, designation, address) VALUES ('ravi@gmail.com', 'ravi', 'caption','India');
