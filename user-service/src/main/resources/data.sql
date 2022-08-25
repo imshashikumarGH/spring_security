7c25bf387ed15a39a24e29c1470222e954788ad8
imshashikumarGH <imshashikumar01@gmail.com>
2022-08-25 23:16:41 +0530

    Feature DB creation : Insert SQL commands in data.sql

--- user-service/src/main/resources/data.sql
+++ user-service/src/main/resources/data.sql
@@ -1,2 +1,7 @@
-INSERT INTO person (p_id, name, designation, address) VALUES ('shashi@gmail.com', 'shashi', 'SE', 'USA');
-INSERT INTO person (p_id, name, designation, address) VALUES ('ravi@gmail.com', 'ravi', 'caption', 'India');
+INSERT INTO address (id, city, country) VALUES (101, 'Delhi', 'India');
+INSERT INTO address (id, city, country) VALUES (102, 'Vellore', 'India');
+
+INSERT INTO person (p_id, name, designation, address_id) VALUES ('shashi@gmail.com', 'shashi', 'SE', 101);
+INSERT INTO person (p_id, name, designation, address_id) VALUES ('ravi@gmail.com', 'ravi', 'caption', 102);
+
+
