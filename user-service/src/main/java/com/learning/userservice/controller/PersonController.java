6bec6fab02ec160b7b3d82ba189db44aa551cbef
imshashikumarGH <imshashikumar01@gmail.com>
2022-08-26 20:21:40 +0530

    Feature Spring Layer : Adding Controller layer

---
+++ user-service/src/main/java/com/learning/userservice/controller/PersonController.java
@@ -0,0 +1,27 @@
+package com.learning.userservice.controller;
+
+import com.learning.userservice.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.websocket.server.PathParam;
+
+
+@RestController()
+@RequestMapping("/person")
+public class PersonController {
+
+    @Autowired
+    PersonService personService;
+
+    @PostMapping(value = "/updateAddress")
+    public ResponseEntity<?> updateAddressCity(@PathParam("personId") String personId, @PathParam("city") String city) {
+
+        return new ResponseEntity<>(personService.updateCityAddressDaoService(personId, city), HttpStatus.OK);
+
+    }
+}
