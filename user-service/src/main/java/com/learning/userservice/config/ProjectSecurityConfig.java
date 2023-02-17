e9a8931109bfb4286db38342d0d045af0469ab55
Shashi Kumar <111182786+imshashikumarGH@users.noreply.github.com>
2023-02-17 15:04:19 +0530

    Spring security Default impl with requestMatcher based Auth feature (#5)
    * Spring Security Config : Override the Default behaviour of Spring Security
    
    * Spring Security Config : RequestMatchers impl for filter the request for suth or no auth required to access API

--- user-service/src/main/java/com/learning/userservice/config/ProjectSecurityConfig.java
+++ user-service/src/main/java/com/learning/userservice/config/ProjectSecurityConfig.java
@@ -1,19 +1,40 @@
 package com.learning.userservice.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Configuration
 public class ProjectSecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        ((ExpressionUrlAuthorizationConfigurer.AuthorizedUrl) http.authorizeRequests().anyRequest()).authenticated();
-        http.formLogin();
-        http.httpBasic();
-        return (SecurityFilterChain) http.build();
+        http.authorizeHttpRequests()
+                .requestMatchers(new RequestMatcher() {
+                    @Override
+                    public boolean matches(HttpServletRequest request) {
+                        if (request.getRequestURI().matches("/person/getAllPersonWithAddress"))
+                            return true;
+                        return false;
+                    }
+                })
+                .authenticated()
+                .requestMatchers(new RequestMatcher() {
+                    @Override
+                    public boolean matches(HttpServletRequest request) {
+                        if (request.getRequestURI().matches("/swagger-ui/index.html"))
+                            return true;
+                        return false;
+                    }
+                }).permitAll()
+
+                .and().formLogin()
+                .and().httpBasic();
+        return http.build();
+
+
     }
 }
