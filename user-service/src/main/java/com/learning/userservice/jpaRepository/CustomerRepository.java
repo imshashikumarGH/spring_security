a34f16cd5a2ef3ffe5479a7b2549efe18627f5c8
imshashikumarGH <imshashikumar01@gmail.com>
2023-03-16 16:23:44 +0530

    Feature BasicSpringSecurity : Custom user Table

---
+++ user-service/src/main/java/com/learning/userservice/jpaRepository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.learning.userservice.jpaRepository;
+
+import com.learning.userservice.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, String> {
+}
