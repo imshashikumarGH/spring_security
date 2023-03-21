1e214899dd4d2aae1a5a5c3470ce25817350967a
imshashikumarGH <imshashikumar01@gmail.com>
2023-03-21 14:55:46 +0530

    Feature CROS_CSRF_Impl : Adding Authority Table and ManyToMany relationShip with customer

---
+++ user-service/src/main/java/com/learning/userservice/jpaRepository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.learning.userservice.jpaRepository;
+
+import com.learning.userservice.entity.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
