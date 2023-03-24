6954e0ec2a8d27150d84b85530da529d7c3faa6e
imshashikumarGH <imshashikumar01@gmail.com>
2023-03-24 17:24:44 +0530

    Feature Method Level Security : Using Method level security validating role and id

--- user-service/src/main/java/com/learning/userservice/service/PersonService.java
+++ user-service/src/main/java/com/learning/userservice/service/PersonService.java
@@ -1,11 +1,14 @@
 package com.learning.userservice.service;
 
 import com.learning.userservice.model.Person;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.util.List;
 
 public interface PersonService {
+    @PreAuthorize("#personId == authentication.principal")
     Person updateCityAddressDaoService(String personId, String city);
 
+    @PreAuthorize("hasRole('ROOT')")
     List<Person> getAllPersonWithAddressService();
 }
