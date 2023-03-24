b58ab95be3f0383212d2ed5b99c8cbbbe57e6291
imshashikumarGH <imshashikumar01@gmail.com>
2023-03-24 17:38:15 +0530

    Feature Method Level Security :  PostFilter on collection return of a method

--- user-service/src/main/java/com/learning/userservice/service/PersonService.java
+++ user-service/src/main/java/com/learning/userservice/service/PersonService.java
@@ -1,6 +1,7 @@
 package com.learning.userservice.service;
 
 import com.learning.userservice.model.Person;
+import org.springframework.security.access.prepost.PostFilter;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.util.List;
@@ -9,6 +10,11 @@ public interface PersonService {
     @PreAuthorize("#personId == authentication.principal")
     Person updateCityAddressDaoService(String personId, String city);
 
-    @PreAuthorize("hasRole('ROOT')")
+
+    //Filter only works on collections
+    //preFilter on collection parameter
+    //postFilter on collection return
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostFilter("filterObject.pId == authentication.principal")
     List<Person> getAllPersonWithAddressService();
 }
