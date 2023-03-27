fb6ffa8e861c76bde15374d9a2e6b319cc5ca0d2
imshashikumarGH <imshashikumar01@gmail.com>
2023-03-27 22:54:15 +0530

    Basic Oauth2 Impl:code refactor

--- user-service-oauth2/src/main/java/com/learning/userserviceoauth2/config/OAuth2GithubConfig.java
+++ user-service-oauth2/src/main/java/com/learning/userserviceoauth2/config/OAuth2GithubConfig.java
@@ -24,6 +24,6 @@ public class OAuth2GithubConfig {
 //
 //    private ClientRegistration clientRegistration() {
 //        return CommonOAuth2Provider.GITHUB.getBuilder("github").clientId("28c15493bdfc7a17b54c")
-//                .clientSecret("2249ce5e4114d3294415cd93f0b1c701a7abca99").build();
+//                .clientSecret("").build();
 //    }
 }
