f43b8b87002387433ccdb88d9a4a164156941094
imshashikumarGH <imshashikumar01@gmail.com>
2023-03-27 12:40:20 +0530

    Basic Oauth2 Impl: Basic Spring Boot project with OAuth2 Client dependency

---
+++ user-service-oauth2/src/main/java/com/learning/userserviceoauth2/UserServiceOauth2Application.java
@@ -0,0 +1,13 @@
+package com.learning.userserviceoauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceOauth2Application.class, args);
+	}
+
+}
---
+++ user-service-oauth2/src/test/java/com/learning/userserviceoauth2/UserServiceOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.userserviceoauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceOauth2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
