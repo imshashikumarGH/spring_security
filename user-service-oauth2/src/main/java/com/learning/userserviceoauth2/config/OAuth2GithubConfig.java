4a986371a46012f6f2970e113362637f2c9360c5
imshashikumarGH <imshashikumar01@gmail.com>
2023-03-27 21:37:00 +0530

    Basic Oauth2 Impl: Using Spring auto Configuration property file to add SecurityFilterChain to map oauth2 and adding Client details - 'GITHUB' - in ClientRegistrationRepository

--- user-service-oauth2/src/main/java/com/learning/userserviceoauth2/config/OAuth2GithubConfig.java
+++ user-service-oauth2/src/main/java/com/learning/userserviceoauth2/config/OAuth2GithubConfig.java
@@ -3,10 +3,6 @@ package com.learning.userserviceoauth2.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,14 +16,14 @@ public class OAuth2GithubConfig {
         return httpSecurity.build();
     }
 
-    @Bean
-    public ClientRegistrationRepository clientRegistrationRepository() {
-        ClientRegistration clientRegistration = clientRegistration();
-        return new InMemoryClientRegistrationRepository(clientRegistration);
-    }
-
-    private ClientRegistration clientRegistration() {
-        return CommonOAuth2Provider.GITHUB.getBuilder("github").clientId("28c15493bdfc7a17b54c")
-                .clientSecret("2249ce5e4114d3294415cd93f0b1c701a7abca99").build();
-    }
+//    @Bean
+//    public ClientRegistrationRepository clientRegistrationRepository() {
+//        ClientRegistration clientRegistration = clientRegistration();
+//        return new InMemoryClientRegistrationRepository(clientRegistration);
+//    }
+//
+//    private ClientRegistration clientRegistration() {
+//        return CommonOAuth2Provider.GITHUB.getBuilder("github").clientId("28c15493bdfc7a17b54c")
+//                .clientSecret("2249ce5e4114d3294415cd93f0b1c701a7abca99").build();
+//    }
 }
