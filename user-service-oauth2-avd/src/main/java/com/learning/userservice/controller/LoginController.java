5625af8d71289f0331d107dce5cb1331e32d3045
imshashikumarGH <imshashikumar01@gmail.com>
2023-03-28 20:05:53 +0530

    Feature Oauth2 impl : Code refactor no need to maintain user registration with have oauth server for that

--- user-service-oauth2-avd/src/main/java/com/learning/userservice/controller/LoginController.java
+++ user-service-oauth2-avd/src/main/java/com/learning/userservice/controller/LoginController.java
@@ -1,13 +1,10 @@
 package com.learning.userservice.controller;
 
-import com.learning.userservice.exception.BadRequestException;
-import com.learning.userservice.model.CustomerRequest;
 import com.learning.userservice.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,17 +18,17 @@ public class LoginController {
     @Autowired
     LoginService loginService;
 
-    @PostMapping(value = "/register")
-    public ResponseEntity<Map> registerCustomer(@RequestBody CustomerRequest customerRequest) {
-        if (customerRequest.getEmail().equals("")) {
-            throw new BadRequestException("bad request");
-        }
-        loginService.registerUser(customerRequest);
-
-        Map<String, String> map = new HashMap<>();
-        map.put("Message", "User Registered");
-        return new ResponseEntity(map, HttpStatus.CREATED);
-    }
+//    @PostMapping(value = "/register")
+//    public ResponseEntity<Map> registerCustomer(@RequestBody CustomerRequest customerRequest) {
+//        if (customerRequest.getEmail().equals("")) {
+//            throw new BadRequestException("bad request");
+//        }
+//        loginService.registerUser(customerRequest);
+//
+//        Map<String, String> map = new HashMap<>();
+//        map.put("Message", "User Registered");
+//        return new ResponseEntity(map, HttpStatus.CREATED);
+//    }
 
     @PostMapping(value = "/login")
     public ResponseEntity<Map> loginUser() {
--- user-service-oauth2-avd/src/main/java/com/learning/userservice/dao/impl/LoginDaoImpl.java
+++ user-service-oauth2-avd/src/main/java/com/learning/userservice/dao/impl/LoginDaoImpl.java
@@ -1,42 +1,17 @@
 package com.learning.userservice.dao.impl;
 
 import com.learning.userservice.dao.LoginDao;
-import com.learning.userservice.entity.Authority;
-import com.learning.userservice.entity.Customer;
-import com.learning.userservice.jpaRepository.AuthorityRepository;
-import com.learning.userservice.jpaRepository.CustomerRepository;
 import com.learning.userservice.model.CustomerRequest;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Repository;
-
-import java.util.Arrays;
 
 @Repository
 @Slf4j
 public class LoginDaoImpl implements LoginDao {
-    @Autowired
-    CustomerRepository customerRepository;
 
-    //to encrypt the password of newly register User
-    @Autowired
-    PasswordEncoder passwordEncoder;
-
-    @Autowired
-    AuthorityRepository authorityRepository;
 
 
     @Override
     public void registerUserDao(CustomerRequest customerRequest) {
-        //Encrypting the password of newly register User
-        String hashPwd = passwordEncoder.encode(customerRequest.getPassword());
-        Authority authorities = authorityRepository.findById(customerRequest.getRole()).orElse(authorityRepository.findById("user").get());
-        Customer customer = Customer.builder()
-                .email(customerRequest.getEmail())
-                .password(hashPwd)
-                .authorities(Arrays.asList(authorities))
-                .build();
-        customerRepository.save(customer);
     }
 }
